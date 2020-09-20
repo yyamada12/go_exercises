a84077fed22e57ba6e85ed2c5a9a841e476c57a5
yyamada12 <12yacropolisy@gmail.com>
2020-09-20 18:13:26 +0900

    update ex4.4

--- ch04/ex04/rotate.go
+++ ch04/ex04/rotate.go
@@ -9,42 +9,42 @@ func main() {
 
 }
 
-func rotate(s []int, n int) []int {
-	return swapRotate(s, 0, len(s), n)
+func rotate(s []int, n int) {
+	swapRotate(s, 0, len(s), n)
 }
 
 // A B -> B A
 // A: [start, start+1, ... , start+n-1]
 // B: [start+n, start+n+1, ... , start+len-1]
-func swapRotate(s []int, start, len, n int) []int {
+func swapRotate(s []int, start, len, n int) {
 	if n == 0 || n == len {
-		return s
+		return
 	}
 
 	// len(A) == len(B)
 	if n == len-n {
-		return blockSwap(s, start, start+n, n)
+		blockSwap(s, start, start+n, n)
+		return
 	}
 
 	if n < len-n {
 		// if len(A) < len(B)
 		// A BL BR -> BR BL A
-		s = blockSwap(s, start, start+len-n, n)
+		blockSwap(s, start, start+len-n, n)
 		// BR BL A -> BL BR A (= B A)
-		return swapRotate(s, start, len-n, n)
+		swapRotate(s, start, len-n, n)
 	} else {
 		// if len(A) > len(B)
 		// AL AR B -> B AR AL
-		s = blockSwap(s, start, n, len-n)
+		blockSwap(s, start, n, len-n)
 		// B AR AL -> B AL AR (= B A)
-		return swapRotate(s, start+len-n, n-(len-n), n)
+		swapRotate(s, start+len-n, n-(len-n), n)
 	}
 }
 
 // swap [l, l+1, ... , l+n] and [r, r+1, ... , r+n]
-func blockSwap(s []int, l, r, n int) []int {
+func blockSwap(s []int, l, r, n int) {
 	for i := 0; i < n; i++ {
 		s[l+i], s[r+i] = s[r+i], s[l+i]
 	}
-	return s
 }
