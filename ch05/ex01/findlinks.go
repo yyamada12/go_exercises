75fbab6eee50cb171282880ece54477f6b1d91c7
yyamada12 <12yacropolisy@gmail.com>
2020-09-29 20:38:14 +0900

    ex5.1

---
+++ ch05/ex01/findlinks.go
@@ -0,0 +1,68 @@
+// Findlinks prints the links in an HTML document read from standard input.
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks: %v\n", err)
+		os.Exit(1)
+	}
+	for _, link := range visit(nil, doc) {
+		fmt.Println(link)
+	}
+}
+
+// visit appends to links each link found in n and returns the result.
+func visit(links []string, n *html.Node) []string {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				links = append(links, a.Val)
+			}
+		}
+	}
+	if c := n.FirstChild; c != nil {
+		links = visit(links, c)
+	}
+	if c := n.NextSibling; c != nil {
+		links = visit(links, c)
+	}
+	return links
+}
+
+/*
+//!+html
+package html
+
+type Node struct {
+	Type                    NodeType
+	Data                    string
+	Attr                    []Attribute
+	FirstChild, NextSibling *Node
+}
+
+type NodeType int32
+
+const (
+	ErrorNode NodeType = iota
+	TextNode
+	DocumentNode
+	ElementNode
+	CommentNode
+	DoctypeNode
+)
+
+type Attribute struct {
+	Key, Val string
+}
+
+func Parse(r io.Reader) (*Node, error)
+//!-html
+*/
---
+++ ch05/ex01/findlinks_test.go
@@ -0,0 +1,39 @@
+// Findlinks prints the links in an HTML document read from standard input.
+package main
+
+import (
+	"log"
+	"os"
+	"sort"
+	"testing"
+
+	"golang.org/x/net/html"
+)
+
+func Test_visit(t *testing.T) {
+	// setup
+	fp, err := os.Open("sample.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fp.Close()
+	doc, err := html.Parse(fp)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// execute
+	got := visit(nil, doc)
+
+	// assert
+	want := []string{"https://support.eji.org/give/153413/#!/donation/checkout", "/", "/doc/", "/pkg/", "/project/", "/help/", "/blog/", "https://play.golang.org/", "/dl/", "https://tour.golang.org/", "https://blog.golang.org/", "/doc/copyright.html", "/doc/tos.html", "http://www.google.com/intl/en/policies/privacy/", "http://golang.org/issues/new?title=x/website:", "https://google.com"}
+
+	sort.Strings(got)
+	sort.Strings(want)
+	for i, g := range got {
+		if g != want[i] {
+			t.Errorf("got %v, want %v", g, want[i])
+		}
+	}
+
+}
