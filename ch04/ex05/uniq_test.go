ec68329a38599770585a813d9ab2f87bbb38eb0e
yyamada12 <12yacropolisy@gmail.com>
2020-10-01 09:54:34 +0900

    update ex4.5

---
+++ ch04/ex05/uniq_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_uniq(t *testing.T) {
+	type args struct {
+		s []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{"only one", args{[]string{"hoge"}}, []string{"hoge"}},
+		{"same words", args{[]string{"hoge", "hoge", "hoge"}}, []string{"hoge"}},
+		{"many words", args{[]string{"hoge", "piyo", "piyo"}}, []string{"hoge", "piyo"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := uniq(tt.args.s); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("uniq() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
