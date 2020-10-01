56421c7aa6ee38611461d4985b17b28a7bb49339
yyamada12 <12yacropolisy@gmail.com>
2020-10-01 13:15:37 +0900

    update ex4.7

--- ch04/ex07/reverse.go
+++ ch04/ex07/reverse.go
@@ -1,9 +1,4 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
-// See page 86.
-
-// Rev reverses a slice.
+// reverseString reverses rune in a byte slice.
 package main
 
 import (
---
+++ ch04/ex07/reverse_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_reverseString(t *testing.T) {
+	tests := []struct {
+		name string
+		arg  []byte
+		want []byte
+	}{
+		{"only ASCII", []byte("hoge 123_"), []byte("_321 egoh")},
+		{"only 3-byte", []byte("ほげほげ"), []byte("げほげほ")},
+		{"mix 1-byte to 4-byte", []byte("a©世🤔"), []byte("🤔世©a")},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			reverseString(tt.arg)
+			if !reflect.DeepEqual(tt.arg, tt.want) {
+				t.Errorf("got %q, want %q", tt.arg, tt.want)
+			}
+		})
+	}
+}
