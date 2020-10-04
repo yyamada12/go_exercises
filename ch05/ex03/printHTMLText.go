e54af746a2de8391cb2eeade147bb44e9a1f11d1
yyamada12 <12yacropolisy@gmail.com>
2020-10-04 21:56:40 +0900

    ex5.3

---
+++ ch05/ex03/printHTMLText.go
@@ -0,0 +1,44 @@
+// printHTMLText prints text in an HTML document read from standard input.
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks: %v\n", err)
+		os.Exit(1)
+	}
+	for _, link := range visit(nil, doc) {
+		_ = len(link)
+		fmt.Println(link)
+	}
+}
+
+// visit appends to texts each text found in n and returns the result.
+func visit(links []string, n *html.Node) []string {
+	if n.Type == html.TextNode {
+		for _, line := range strings.Split(n.Data, "\n") {
+			text := strings.TrimSpace(line)
+			if text != "" {
+				links = append(links, text)
+			}
+		}
+	}
+	// skip if n is script or style tag
+	if !(n.Type == html.ElementNode) || (n.Data != "script" && n.Data != "style") {
+		if c := n.FirstChild; c != nil {
+			links = visit(links, c)
+		}
+	}
+	if c := n.NextSibling; c != nil {
+		links = visit(links, c)
+	}
+	return links
+}
---
+++ ch05/ex03/printHTMLText_test.go
@@ -0,0 +1,39 @@
+// Findlinks prints the links in an HTML document read from standard input.
+package main
+
+import (
+	"log"
+	"os"
+	"sort"
+	"testing"
+
+	"golang.org/x/net/html"
+)
+
+func Test_visit(t *testing.T) {
+	// setup
+	fp, err := os.Open("sample.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fp.Close()
+	doc, err := html.Parse(fp)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// execute
+	got := visit(nil, doc)
+
+	// assert
+	want := []string{"The Go Programming Language", "Black Lives Matter.", "Support the Equal Justice Initiative.", "Documents", "Packages", "The Project", "Help", "Blog", "Play", "Search", "Go is an open source programming language that makes it easy to", "build", "simple", ",", "reliable", ", and", "efficient", "software.", "Download Go", "Binary distributions available for", "Linux, macOS, Windows, and more.", "Try Go", "Open in Playground", "// You can edit this code!", "// Click here and start typing.", "package main", "import \"fmt\"", "func main() {", "fmt.Println(\"Hello, 世界\")", "}", "Hello, 世界", "Hello, World!", "Conway's Game of Life", "Fibonacci Closure", "Peano Integers", "Concurrent pi", "Concurrent Prime Sieve", "Peg Solitaire Solver", "Tree Comparison", "Run", "Share", "Tour", "Featured articles", "Read more >", "Featured video", "Copyright", "Terms of Service", "Privacy Policy", "Report a website issue", "Supported by Google"}
+
+	sort.Strings(got)
+	sort.Strings(want)
+	for i, g := range got {
+		if g != want[i] {
+			t.Errorf("got %v, want %v", g, want[i])
+		}
+	}
+
+}
