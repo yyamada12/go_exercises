e8e1a14fcb5d19a64c12b159aba3c1b10037481c
yyamada12 <12yacropolisy@gmail.com>
2020-10-25 22:45:35 +0900

    ex7.14

---
+++ ch07/ex14/check.go
@@ -0,0 +1,70 @@
+package eval
+
+import (
+	"fmt"
+	"strings"
+)
+
+func (v Var) Check(vars map[Var]bool) error {
+	vars[v] = true
+	return nil
+}
+
+func (literal) Check(vars map[Var]bool) error {
+	return nil
+}
+
+func (u unary) Check(vars map[Var]bool) error {
+	if !strings.ContainsRune("+-", u.op) {
+		return fmt.Errorf("unexpected unary op %q", u.op)
+	}
+	return u.x.Check(vars)
+}
+
+func (b binary) Check(vars map[Var]bool) error {
+	if !strings.ContainsRune("+-*/", b.op) {
+		return fmt.Errorf("unexpected binary op %q", b.op)
+	}
+	if err := b.x.Check(vars); err != nil {
+		return err
+	}
+	return b.y.Check(vars)
+}
+
+func (c call) Check(vars map[Var]bool) error {
+	arity, ok := numParams[c.fn]
+	if !ok {
+		return fmt.Errorf("unknown function %q", c.fn)
+	}
+	if len(c.args) != arity {
+		return fmt.Errorf("call to %s has %d args, want %d",
+			c.fn, len(c.args), arity)
+	}
+	for _, arg := range c.args {
+		if err := arg.Check(vars); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+var numParams = map[string]int{"pow": 2, "sin": 1, "sqrt": 1}
+
+func (a aggregate) Check(vars map[Var]bool) error {
+	arity, ok := minNumParams[a.fn]
+	if !ok {
+		return fmt.Errorf("unknown aggregate %q", a.fn)
+	}
+	if len(a.args) < arity {
+		return fmt.Errorf("operate %s with %d args, want at least %d",
+			a.fn, len(a.args), arity)
+	}
+	for _, arg := range a.args {
+		if err := arg.Check(vars); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+var minNumParams = map[string]int{"min": 2, "max": 2}
