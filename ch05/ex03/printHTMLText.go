b65846da34bd69fa8db1fece9d648190377aaea8
yyamada12 <12yacropolisy@gmail.com>
2020-10-30 13:11:26 +0900

    update ex5.3

--- ch05/ex03/printHTMLText.go
+++ ch05/ex03/printHTMLText.go
@@ -36,8 +36,8 @@ func visit(links []string, n *html.Node) []string {
 			links = visit(links, c)
 		}
 	}
-	if c := n.NextSibling; c != nil {
-		links = visit(links, c)
+	if next := n.NextSibling; next != nil {
+		links = visit(links, next)
 	}
 	return links
 }
