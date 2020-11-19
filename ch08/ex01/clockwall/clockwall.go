4f2b37e248fc1600731e121f8e2b637f518e32e8
yyamada12 <12yacropolisy@gmail.com>
2020-11-19 12:28:55 +0900

    update ex8.1

--- ch08/ex01/clockwall/clockwall.go
+++ ch08/ex01/clockwall/clockwall.go
@@ -9,25 +9,38 @@ import (
 	"strings"
 )
 
+type result struct {
+	tz   string
+	time string
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		printUsage()
 		return
 	}
+	clocks := map[string]string{}
+	results := make(chan result)
+	tzs := []string{}
 	for _, arg := range os.Args[1:] {
 		a := strings.Split(arg, "=")
-		if len(a) < 2 {
+		if len(a) != 2 {
 			log.Fatalf("invalid arg: %s", arg)
 		}
-		go clock(a[0], a[1])
+		tzs = append(tzs, a[0])
+		go clock(a[0], a[1], results)
 	}
 	for {
+		r := <-results
+		clocks[r.tz] = r.time
+		printClocks(clocks, tzs)
 	}
 }
 
 func printUsage() {
 	fmt.Println(`USAGE:
-	go run clockwall.go [CLOCK NAME]=[ADDR], [[CLOCK NAME]=[ADDR], ...]
+	go run clockwall.go [CLOCK NAME]=[ADDR] [[CLOCK NAME]=[ADDR] ...]
+	ex) go run clockwall.go Tokyo=localhost:8000 NewYork=localhost:8010
 
 CLOCK NAME:
 	give the clock name	like "Tokyo", "NewYork"
@@ -35,7 +48,17 @@ ADDR:
 	the addr to connect like "localhost:8000"`)
 }
 
-func clock(tz, addr string) {
+func printClocks(clocks map[string]string, tzs []string) {
+	var times []string
+	for _, tz := range tzs {
+		if time, ok := clocks[tz]; ok {
+			times = append(times, tz+" "+time)
+		}
+	}
+	fmt.Print("\r" + strings.Join(times, "    "))
+}
+
+func clock(tz, addr string, results chan result) {
 	conn, err := net.Dial("tcp", addr)
 	if err != nil {
 		log.Fatal(err)
@@ -43,7 +66,7 @@ func clock(tz, addr string) {
 	defer conn.Close()
 	scanner := bufio.NewScanner(conn)
 	for scanner.Scan() {
-		fmt.Println(tz + ": " + scanner.Text())
+		results <- result{tz, scanner.Text()}
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
