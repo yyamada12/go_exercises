010e603a6e03ec6b11751f1c7b27b8d6913d6690
yyamada12 <12yacropolisy@gmail.com>
2020-12-26 17:53:25 +0900

    wip ex11.7

--- ch11/ex07/intset_test.go
+++ ch11/ex07/intset_test.go
@@ -19,7 +19,7 @@ func words_Add(b *testing.B, inputs []int) {
 
 func map_Add(b *testing.B, inputs []int) {
 	for i := 0; i < b.N; i++ {
-		var x IntSet
+		x := NewMapIntSet()
 		for _, input := range inputs {
 			x.Add(input)
 		}
@@ -38,40 +38,41 @@ func Benchmark_Add(b *testing.B) {
 		words_Add(b, inputs)
 	})
 	b.Run("map", func(b *testing.B) {
-		words_Add(b, inputs)
+		map_Add(b, inputs)
 	})
-
 }
 
-func Test_wordsAndMap(t *testing.T) {
+func words_UnionWith(b *testing.B, x, y *IntSet) {
+	for i := 0; i < b.N; i++ {
+		x.UnionWith(y)
+	}
+}
+
+func map_UnionWith(b *testing.B, x, y *MapIntSet) {
+	for i := 0; i < b.N; i++ {
+		x.UnionWith(y)
+	}
+}
+
+func Benchmark_UnionWith(b *testing.B) {
+	seed := time.Now().UTC().UnixNano()
+	b.Logf("Random seed: %d", seed)
+	rng := rand.New(rand.NewSource(seed))
 	var x, y IntSet
 	x2 := NewMapIntSet()
 	y2 := NewMapIntSet()
-	x.Add(1)
-	x2.Add(1)
-	x.Add(144)
-	x2.Add(144)
-	x.Add(9)
-	x2.Add(9)
-	if x.String() != x2.String() {
-		t.Errorf("x got %s, want %s", x.String(), x2.String())
+	for i := 0; i < 100000; i++ {
+		r1 := rng.Intn(10000)
+		r2 := rng.Intn(10000)
+		x.Add(r1)
+		y.Add(r2)
+		x2.Add(r1)
+		y2.Add(r2)
 	}
-
-	y.Add(9)
-	y2.Add(9)
-	y.Add(42)
-	y2.Add(42)
-
-	x.UnionWith(&y)
-	x2.UnionWith(y2)
-	if x.String() != x2.String() {
-		t.Errorf("x union with y got %s, want %s", x.String(), x2.String())
-	}
-
-	if x.Has(9) != x2.Has(9) {
-		t.Errorf("x.Has(9) got %t, want %t", x.Has(9), x2.Has(9))
-	}
-	if x.Has(123) != x2.Has(123) {
-		t.Errorf("x.Has(123) got %t, want %t", x.Has(123), x2.Has(123))
-	}
+	b.Run("words", func(b *testing.B) {
+		words_UnionWith(b, &x, &y)
+	})
+	b.Run("map", func(b *testing.B) {
+		map_UnionWith(b, x2, y2)
+	})
 }
