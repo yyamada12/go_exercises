aa5489256f1d80d0063aed1b1d0c1bd60719ed2f
yyamada12 <12yacropolisy@gmail.com>
2020-12-27 21:13:21 +0900

    wip ex11.7

--- ch11/ex07/intset_test.go
+++ ch11/ex07/intset_test.go
@@ -6,8 +6,6 @@ import (
 	"time"
 )
 
-var out IntSet
-
 func words_Add(b *testing.B, inputs []int) {
 	for i := 0; i < b.N; i++ {
 		var x IntSet
@@ -74,5 +72,38 @@ func Benchmark_UnionWith(b *testing.B) {
 	})
 	b.Run("map", func(b *testing.B) {
 		map_UnionWith(b, x2, y2)
+	})
+}
+
+func words_Has(b *testing.B, x *IntSet, e int) {
+	for i := 0; i < b.N; i++ {
+		x.Has(e)
+	}
+}
+
+func map_Has(b *testing.B, x *MapIntSet, e int) {
+	for i := 0; i < b.N; i++ {
+		x.Has(e)
+	}
+}
+
+func Benchmark_Has(b *testing.B) {
+	seed := time.Now().UTC().UnixNano()
+	b.Logf("Random seed: %d", seed)
+	rng := rand.New(rand.NewSource(seed))
+	var x IntSet
+	x2 := NewMapIntSet()
+	for i := 0; i < 100000; i++ {
+		r := rng.Intn(10000)
+		x.Add(r)
+		x2.Add(r)
+	}
+	e := rng.Intn(10000)
+
+	b.Run("words", func(b *testing.B) {
+		words_Has(b, &x, e)
+	})
+	b.Run("map", func(b *testing.B) {
+		map_Has(b, x2, e)
 	})
 }
