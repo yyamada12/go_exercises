187c00b845aadb98692d8137688e58c4836fab99
yyamada12 <12yacropolisy@gmail.com>
2021-01-08 13:49:22 +0900

    update ex9.5

--- ch09/ex05/rally.go
+++ ch09/ex05/rally.go
@@ -2,26 +2,28 @@ package main
 
 import (
 	"fmt"
+	"sync/atomic"
 	"time"
 )
 
 func main() {
-	var n int
+	var n int64
 	ping, pong := make(chan struct{}), make(chan struct{})
 	go func() {
 		for {
 			<-ping
-			n++
+			atomic.AddInt64(&n, 1)
 			pong <- struct{}{}
 		}
 	}()
 	go func() {
 		for {
 			<-pong
+			atomic.AddInt64(&n, 1)
 			ping <- struct{}{}
 		}
 	}()
 	ping <- struct{}{}
 	time.Sleep(1 * time.Second)
-	fmt.Printf("%d rallies during 1 second\n", n)
+	fmt.Printf("%d rallies during 1 second\n", atomic.LoadInt64(&n))
 }
